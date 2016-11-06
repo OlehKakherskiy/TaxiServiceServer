440679994cb4435b59b1b60f5ca0d3bb1c91b498
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-11-07 00:34:54 +0200

    added searching all orders by status method declaration

--- src/main/java/ua/kpi/mobiledev/repository/OrderRepository.java
+++ src/main/java/ua/kpi/mobiledev/repository/OrderRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ua.kpi.mobiledev.domain.Order;
 
+import java.util.List;
+
 /**
  * Created by Oleg on 06.11.2016.
  */
 @Repository
-public interface OrderRepository extends CrudRepository<Order, Integer> {
+public interface OrderRepository extends CrudRepository<Order, Long> {
+
+    List<Order> getAllByOrderStatus(Order.OrderStatus orderStatus);
 }
