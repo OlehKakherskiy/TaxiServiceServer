5fc74e32b9ecc1b7893dffb4c1482e746c69fcee
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-11-06 02:49:35 +0200

    added bd model, xml with persistence unit and main class for testing app in manual mode

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import ua.kpi.mobiledev.domain.MobileNumber;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ * Created by Oleg on 06.11.2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = emf.createEntityManager();
+        MobileNumber mobileNumber = new MobileNumber(null, null, null);
+//        EntityTransaction transaction = em.getTransaction();
+//        transaction.begin();
+//        User user = new User(null, "name", "email", new ArrayList<>());
+//        user.getMobileNumbers().addAll(Arrays.asList(new MobileNumber(null, "+380935705681", user), new MobileNumber(null, "+380975106619", user)));
+//        System.out.println("user before persist = " + user);
+//        em.persist(user);
+//        em.flush();
+//        System.out.println("user after persist = " + user);
+//        System.out.println("user after find = " + em.find(User.class, user.getId()));
+//        transaction.commit();
+//
+//        EntityTransaction transaction1 = em.getTransaction();
+//        transaction1.begin();
+//        TaxiDriver taxiDriver = new TaxiDriver(null, "name2", "email2", new ArrayList<>(), new Car(null, "model", "manufacturer", "plateNum", 6, Car.CarType.MINIBUS));
+//        taxiDriver.getMobileNumbers().addAll(Arrays.asList(new MobileNumber(null, "+380935705681(1)", user), new MobileNumber(null, "+380975106619(1)", taxiDriver)));
+//        System.out.println("taxiDriver before persist= " + taxiDriver);
+//        em.persist(taxiDriver);
+//        em.flush();
+//        System.out.println("taxiDriver after persist= " + taxiDriver);
+//        System.out.println("em.find(T) = " + em.find(TaxiDriver.class, taxiDriver.getId()));
+//        transaction1.commit();
+
+        emf.close();
+        em.close();
+    }
+}
