241ace49045554e12bb74b41b50f178e8c0e86d7
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-11-28 01:02:53 +0200

    added mapping method from entity to dto. Added 'price' field because it's described in API. It's used just for entity-dto mapping

--- src/main/java/ua/kpi/mobiledev/domain/dto/OrderDto.java
+++ src/main/java/ua/kpi/mobiledev/domain/dto/OrderDto.java
@@ -2,7 +2,8 @@ package ua.kpi.mobiledev.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NonNull;
+import lombok.NoArgsConstructor;
+import ua.kpi.mobiledev.domain.Order;
 
 import javax.validation.Valid;
 import javax.validation.constraints.Future;
@@ -10,10 +11,25 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class OrderDto {
+
+    public static OrderDto from(Order order) {
+        if (Objects.isNull(order)) {
+            return new OrderDto();
+        }
+        OrderDto orderDto = new OrderDto();
+        orderDto.customerId = order.getCustomer().getId();
+        orderDto.startTime = order.getStartTime();
+        orderDto.startPoint = order.getStartPoint();
+        orderDto.endPoint = order.getEndPoint();
+        orderDto.price = order.getPrice();
+        return orderDto;
+    }
 
     @NotNull
     @Min(0)
@@ -34,4 +50,6 @@ public class OrderDto {
     @NotNull
     @Valid
     private OrderPriceDto orderPrice;
+
+    private Double price;
 }
