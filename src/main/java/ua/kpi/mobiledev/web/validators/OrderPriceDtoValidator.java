038638698ce8ac76e9ff380ed29de99405713194
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-11-27 14:07:40 +0200

    added custom validator, that validates orderPriceDto.

---
+++ src/main/java/ua/kpi/mobiledev/web/validators/OrderPriceDtoValidator.java
@@ -0,0 +1,36 @@
+package ua.kpi.mobiledev.web.validators;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+import ua.kpi.mobiledev.domain.dto.OrderPriceDto;
+
+import java.util.Map;
+import java.util.Set;
+
+public class OrderPriceDtoValidator implements Validator {
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return OrderPriceDto.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        ValidationUtils.rejectIfEmpty(errors, "distance", "distance.required");
+        OrderPriceDto orderDto = (OrderPriceDto) target;
+        if (orderDto.getDistance() <= 0.0) {
+            errors.rejectValue("distance", "distance.illegalValue");
+        }
+        ValidationUtils.rejectIfEmpty(errors, "additionalRequestValueMap", "additionalRequestValueMap.required");
+        Map<Integer, Integer> requirementValueMap = orderDto.getAdditionalRequestValueMap();
+        checkSetForNegativeValuesOrZero(requirementValueMap.keySet(), errors, "additionalRequestValueMap.illegalKey");
+        checkSetForNegativeValuesOrZero(requirementValueMap.keySet(), errors, "additionalRequestValueMap.illegalValue");
+    }
+
+    private void checkSetForNegativeValuesOrZero(Set<Integer> values, Errors errors, String errorKey) {
+        values.stream().filter(value -> value < 0).findFirst().ifPresent(value -> {
+            errors.rejectValue("additionalRequestValueMap", errorKey);
+        });
+    }
+}
