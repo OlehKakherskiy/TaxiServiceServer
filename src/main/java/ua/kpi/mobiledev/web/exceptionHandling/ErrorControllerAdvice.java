5811ac5e09dbc55d7c251d92e91ae94add5049ff
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-11-27 16:33:37 +0200

    added controller advice for centralised exception handling strategy implementing. It handles ConstraintViolationException, maps it to list of FieldError objects and return http bad request. FieldError class represents invalid field information: field name, messageCode, message

---
+++ src/main/java/ua/kpi/mobiledev/web/exceptionHandling/ErrorControllerAdvice.java
@@ -0,0 +1,24 @@
+package ua.kpi.mobiledev.web.exceptionHandling;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.ConstraintViolationException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ErrorControllerAdvice {
+
+    @RequestMapping(produces = "application/json")
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<FieldError>> handleFormValidationException(ConstraintViolationException ex) {
+        return ResponseEntity.badRequest()
+                .body(ex.getConstraintViolations()
+                        .stream()
+                        .map(FieldError::of)
+                        .collect(Collectors.toList()));
+    }
+}
