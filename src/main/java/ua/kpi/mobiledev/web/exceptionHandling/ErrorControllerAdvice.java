e0c88696ca21622f22169b6da8846b35a0972250
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-12-02 01:02:56 +0200

    added NullPointerException handling

--- src/main/java/ua/kpi/mobiledev/web/exceptionHandling/ErrorControllerAdvice.java
+++ src/main/java/ua/kpi/mobiledev/web/exceptionHandling/ErrorControllerAdvice.java
@@ -42,4 +42,10 @@ public class ErrorControllerAdvice {
     public ResponseEntity<ErrorMessage> handleIllegalArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.badRequest().body(new ErrorMessage(ex.getLocalizedMessage()));
     }
+
+    @RequestMapping(produces = "application/json")
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<ErrorMessage> handleNPE(NullPointerException ex) {
+        return ResponseEntity.badRequest().body(new ErrorMessage(ex.getLocalizedMessage()));
+    }
 }
