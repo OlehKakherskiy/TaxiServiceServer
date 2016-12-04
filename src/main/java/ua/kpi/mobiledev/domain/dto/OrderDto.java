cef7b9cdaa6ceaa4316cdc8d50c060310a5a2623
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-12-04 18:08:00 +0200

    changes were performed in OrderDto validation feature: - changed Lombok annotations in OrderDto; - added error codes to each validation point in OrderDto; - added equals and hashcode method in OrderDto; - added custom validator, that validates whether 'startTime' parameter of OrderDto is future time; - custom validator is registered in REST context; - added custom error message for field 'startTime' of OrderDto

--- src/main/java/ua/kpi/mobiledev/domain/dto/OrderDto.java
+++ src/main/java/ua/kpi/mobiledev/domain/dto/OrderDto.java
@@ -2,24 +2,24 @@ package ua.kpi.mobiledev.domain.dto;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 import ua.kpi.mobiledev.domain.Order;
 import ua.kpi.mobiledev.web.localDateTimeMapper.LocalDateTimeDeserializer;
 import ua.kpi.mobiledev.web.localDateTimeMapper.LocalDateTimeSerializer;
 
 import javax.validation.Valid;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.Objects;
 
+@Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class OrderDto {
 
     public static OrderDto from(Order order) {
@@ -35,27 +35,51 @@ public class OrderDto {
         return orderDto;
     }
 
-    @NotNull
-    @Min(0)
+    @NotNull(message = "customerId.required")
+    @Min(value = 0, message = "customerId.invalidValue")
     private Integer customerId;
 
     @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @NotNull
-    @Future
+    @NotNull(message = "startTime.required")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
     private LocalDateTime startTime;
 
-    @NotNull
-    @Size(min = 1)
+    @NotNull(message = "startPoint.required")
+    @Size(min = 1, max = 1000, message = "startPoint.invalidSize")
     private String startPoint;
 
-    @NotNull
-    @Size(min = 1)
+    @NotNull(message = "endPoint.required")
+    @Size(min = 1, max = 1000, message = "endPoint.invalidSize")
     private String endPoint;
 
-    @NotNull
+    @NotNull(message = "orderPrice.required")
     @Valid
     private OrderPriceDto orderPrice;
 
     private Double price;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderDto orderDto = (OrderDto) o;
+
+        if (customerId != null ? !customerId.equals(orderDto.customerId) : orderDto.customerId != null) return false;
+        if (startTime != null ? !startTime.equals(orderDto.startTime) : orderDto.startTime != null) return false;
+        if (startPoint != null ? !startPoint.equals(orderDto.startPoint) : orderDto.startPoint != null) return false;
+        if (endPoint != null ? !endPoint.equals(orderDto.endPoint) : orderDto.endPoint != null) return false;
+        return orderPrice != null ? orderPrice.equals(orderDto.orderPrice) : orderDto.orderPrice == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = customerId != null ? customerId.hashCode() : 0;
+        result = 31 * result + (startTime != null ? startTime.hashCode() : 0);
+        result = 31 * result + (startPoint != null ? startPoint.hashCode() : 0);
+        result = 31 * result + (endPoint != null ? endPoint.hashCode() : 0);
+        result = 31 * result + (orderPrice != null ? orderPrice.hashCode() : 0);
+        return result;
+    }
 }
