92f69f2f544f8a98f91cf34d73b6b79e6d7695ef
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-12-05 22:47:51 +0200

    changed logic of PaymentTypeAdditionalRequirement: from now mapping between payment type and extra money performs. Added unit test coverage

---
+++ src/test/java/ua/kpi/mobiledev/domain/additionalRequirements/PaymentTypeAdditionalRequirementTest.java
@@ -0,0 +1,34 @@
+package ua.kpi.mobiledev.domain.additionalRequirements;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import ua.kpi.mobiledev.domain.AdditionalRequirement;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PaymentTypeAdditionalRequirementTest {
+
+    private static AdditionalRequirement paymentTypeAdditionalRequirement;
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        Map<Integer, Integer> priceMap = new HashMap<>();
+        priceMap.put(1, 0);
+        priceMap.put(2, 5);
+        paymentTypeAdditionalRequirement = new PaymentTypeAdditionalRequirement("", "", Collections.emptyMap(), priceMap);
+    }
+
+    @Test
+    public void addPrice() throws Exception {
+        Assert.assertEquals(0, paymentTypeAdditionalRequirement.addPrice(100, 1), 10e-7);
+    }
+
+    @Test
+    public void addPrice_notExistedKey() throws Exception {
+        Assert.assertEquals(0, paymentTypeAdditionalRequirement.addPrice(100, 5), 10e-7);
+    }
+
+}
