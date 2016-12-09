682df268c2e2ec4581f6380c54f4d684deb4dff6
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-12-10 01:31:30 +0200

    added jpql query, that method should execute

--- src/main/java/ua/kpi/mobiledev/repository/OrderRepository.java
+++ src/main/java/ua/kpi/mobiledev/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package ua.kpi.mobiledev.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ua.kpi.mobiledev.domain.Order;
 
@@ -12,5 +14,6 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
-    List<Order> getAllByOrderStatus(Order.OrderStatus orderStatus);
+    @Query("Select o from Order o where o.orderStatus=:orderStatus")
+    List<Order> getAllByOrderStatus(@Param("orderStatus") Order.OrderStatus orderStatus);
 }
