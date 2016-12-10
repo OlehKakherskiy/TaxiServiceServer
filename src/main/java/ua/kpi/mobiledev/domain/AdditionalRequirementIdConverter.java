592df9bba58e1c03b32c5e77b3f0ad8ff92a2117
Oleh Kakherskyi <olehkakherskiy@gmail.com>
2016-12-10 19:04:15 +0200

    fixed bug with null application context injection. Also there's a problem with null addRequirement id param (unpredictable logic) - also fixed

--- src/main/java/ua/kpi/mobiledev/domain/AdditionalRequirementIdConverter.java
+++ src/main/java/ua/kpi/mobiledev/domain/AdditionalRequirementIdConverter.java
@@ -3,16 +3,19 @@ package ua.kpi.mobiledev.domain;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
+@Component
 @Converter(autoApply = true)
 public class AdditionalRequirementIdConverter implements AttributeConverter<AdditionalRequirement, Integer>, ApplicationContextAware {
 
-    private ApplicationContext applicationContext;
+    private static ApplicationContext applicationContext;
 
     @Override
     public Integer convertToDatabaseColumn(AdditionalRequirement attribute) {
@@ -21,12 +24,15 @@ public class AdditionalRequirementIdConverter implements AttributeConverter<Addi
 
     @Override
     public AdditionalRequirement convertToEntityAttribute(Integer dbData) {
+        if (Objects.isNull(dbData)) {
+            return null;
+        }
         Map<Integer, AdditionalRequirement> additionalRequirements = applicationContext.getBean("additionalRequirements", Map.class);
         return Optional.of(additionalRequirements.get(dbData)).orElseThrow(RuntimeException::new);
     }
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        this.applicationContext = applicationContext;
+        AdditionalRequirementIdConverter.applicationContext = applicationContext;
     }
 }
